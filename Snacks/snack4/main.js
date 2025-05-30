5bf06fa5b469a21ba7a899f0248230fda5bd823b
DavideCriscuolo <davide.alcatel@gmail.com>
2025-05-30 12:27:34 +0200

    Aggiungi file HTML e JavaScript per la raccolta di numeri dispari dall'utente

---
+++ Snacks/snack4/main.js
@@ -0,0 +1,11 @@
+// Crea un array vuoto e chiedi per 6 volte allìutente di inserire un numero se è ddispari inseriscilo nell'array
+
+let numbers = [];
+
+for (let i = 1; i <= 6; i++) {
+  let inputnumb = Number(prompt("Inserisci qualcosa:"));
+  if (inputnumb != null && input % 2 != 0) {
+    numbers.push(input);
+  }
+}
+console.log(numbers);
