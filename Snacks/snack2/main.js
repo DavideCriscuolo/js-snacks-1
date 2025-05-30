35483eaf68d9e2b3a2fa0befdfdce96503338b96
DavideCriscuolo <davide.alcatel@gmail.com>
2025-05-30 11:47:20 +0200

    Aggiungi file HTML e JavaScript per il confronto di due parole

---
+++ Snacks/snack2/main.js
@@ -0,0 +1,15 @@
+/*L'utente inserisce due parole in successione, con due prompt.
+
+il software stampa prima la parola più corta, poi quella più lunga
+*/
+
+let word1 = prompt("Scrivi la prima parola");
+let word2 = prompt("Scrivi la seconda parola");
+
+if (word1.length > word2.length) {
+  console.log(word2, "è la parola piu corta");
+} else if (word1.length < word2.length) {
+  console.log(word2, "è la parola piu lunga");
+} else {
+  console.log("le parole hanno la stessa lunghezza");
+}
